557e324fa265ae8d13746e15a342b59434d2b69b
gaianacheli <gaianacheli04@gmail.com>
2021-10-08 15:38:37 +0200

    esercizio completo

---
+++ main.js
@@ -0,0 +1,17 @@
+//chiedi come si chiama
+ const name = prompt ("Come ti chiami?")
+ console.log (name)
+
+ //chiedi il cognome
+
+ const lastname = prompt ("Qual'è il tuo cognome?")
+ console.log (lastname)
+
+
+ //chiedi cololre preferito
+
+ const color = prompt ("Qual'è il tuo colore preferito?")
+ console.log (color)
+
+
+ document.getElementById ('Gaia').innerHTML="Gaia"+"Nacheli"+"giallo"+"21"
